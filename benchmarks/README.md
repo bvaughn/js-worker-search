adab746e49d9441f8cbdda891817d30876641b08
Brian Vaughn <brian.david.vaughn@gmail.com>
2017-07-16 14:09:38 -0700

    Added some basic benchmarks

---
+++ benchmarks/README.md
@@ -0,0 +1,41 @@
+These benchmark tests measure real usage of js-search.
+They enable simple performance comparisons between the latest released build and the local build.
+To run a benchmark:
+
+```bash
+cd /path/to/js-search
+yarn install
+
+# Build local js-search (to incorporate any changes you've made)
+npm run build
+
+cd ./benchmarks
+yarn install
+
+# Run a benchmark of your choice
+# eg Compare your local build of js-search to the latest released build
+node ./create-index.js
+node ./search.js
+```
+
+You can also compare two local builds of js-search:
+
+```bash
+# Assumes you've run `yarn install` in both directories prior
+
+cd /path/to/js-search
+
+# Make some changes and then build js-search
+npm run build
+
+# Move your build into the benchmarks folder as the 'latest'
+cp -r ./dist ./benchmarks/node_modules/js-search/
+
+# Make more changes and then re-build js-search
+npm run build
+
+# Compare your most recent build to the previous one
+cd ./benchmarks
+node ./create-index.js
+node ./search.js
+```
---
+++ benchmarks/create-index.js
@@ -0,0 +1,53 @@
+const Benchmark = require("benchmark");
+const bb = require("beautify-benchmark");
+const lunr = require("lunr");
+const JsSearchLatest = require("js-worker-search").default;
+const JsSearchLocal = require("../dist/js-worker-search").default;
+
+const indexMode = "EXACT_WORDS";
+
+let books;
+function loadBooks() {
+  const fs = require("fs");
+  fs.readFile("books.json", "utf8", (err, data) => {
+    books = JSON.parse(data).books;
+    runTests();
+  });
+}
+
+function buildIndex(SearchApi) {
+  var search = new SearchApi({ indexMode });
+  books.forEach(book => {
+    const { author, isbn, title } = book;
+    search.indexDocument(isbn, author);
+    search.indexDocument(isbn, title);
+  });
+}
+
+function runTests() {
+  new Benchmark.Suite()
+    .on("cycle", event => {
+      bb.add(event.target);
+    })
+    .on("complete", () => {
+      bb.log();
+    })
+    .add("lunr", () => {
+      var lunrJsIndex = new lunr.Index();
+      lunrJsIndex.field("title");
+      lunrJsIndex.field("author");
+      lunrJsIndex.ref("isbn");
+      for (var i = 0, length = books.length; i < length; i++) {
+        lunrJsIndex.add(books[i]);
+      }
+    })
+    .add("js-worker-search:latest", () => {
+      buildIndex(JsSearchLatest);
+    })
+    .add("js-worker-search:local", () => {
+      buildIndex(JsSearchLocal);
+    })
+    .run({ async: true });
+}
+
+loadBooks();
